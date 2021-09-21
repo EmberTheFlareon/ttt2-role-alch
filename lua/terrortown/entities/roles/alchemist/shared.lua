1fc6ba83f1d91d0d05487fc373bd9fa21b04f13d
EmberTheFlareon <70917055+EmberTheFlareon@users.noreply.github.com>
2021-09-21 05:25:25 -0700

    Timer actually works now!

--- lua/terrortown/entities/roles/alchemist/shared.lua
+++ lua/terrortown/entities/roles/alchemist/shared.lua
@@ -5,34 +5,33 @@
 
 
 if SERVER then
-	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_surv.vmt")
+	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_alch.vtf")
 	AddCSLuaFile()
 
-	util.AddNetworkString("TTT2AlchemistDefaultBrewMode")
 end
 
 function ROLE:PreInitialize()
-	self.color = Color(13, 208, 209, 255)
+	self.color = Color(192, 229, 1, 255)
 	self.abbr = "alch"
-
-	self.score.killsMultiplier = 1
-	self.score.teamKillsMultiplier = -8
 
 	self.unknownTeam = true
 
+	self.score.killsMultiplier = 2
+	self.score.teamKillsMultiplier = -8
 	self.defaultTeam = TEAM_INNOCENT
-	self.defaultEquipment = INNO_EQUIPMENT
+	self.defaultEquipment = SPECIAL_EQUIPMENT
 
 	self.conVarData = {
 		pct = 0.15,  --necessary: percentage of getting this role selected (per player)
 		maximum = 1,  --maximum amount of roles in a round
-		minPlayers = 5, --minimum amount of players until this role is able to get selected
+		minPlayers = 6, --minimum amount of players until this role is able to get selected
+		credits = 1, --How many credits they spawn with.
 		togglable = true, --Makes it so players can toggle getting this role from their own F1 menu.
 	}
 end
 
 function ROLE:Initialize()
-	roles.SetBaseRole(self, ROLE_INNOCENT)
+	roles.SetBaseRole(self, ROLE_DETECTIVE)
 end
 
 if SERVER then
@@ -43,30 +42,18 @@ if SERVER then
 		end
 	end)
 	
-	--This activates when the timer set up below it finishes.
-	--When that happens, it gfives the user a potion.
-	-- Currently, as only one potion has been coded, the random selection code is not present.
+	--This sets up the timer needed to supply a potion after a certain time
 	local function GiveMeAPotion(ply)
-		ply:GiveEquipmentWeapon("weapon_ttt_healpotion")
+		timer.Create( "MakePotion", 30, 5,  function() ply:GiveEquipmentWeapon("weapon_ttt_healpotion") end)
 	end
-	
-	--This tells the game that after a certain amount of time has passed, it should give the player a potion.
-	local function CreatePotionTimer(ply)	
-		--Create a timer that is unique to the player.
-		timer.Create("AlchemistBrewTimer" .. ply:SteamID64(), 20, 3, function()
-			GiveMeAPotion(ply)
-		end)
-	end
-	
-	--Testing purposes only! This is only here to quickly give a potion so that it may be tested.
+	hook.Add( "Initialize", "Timer Example", "MakePotion" )
+
+	--All this really does is set the timer's function to the player.
 	function ROLE:GiveRoleLoadout(ply, isRoleChange)
-		GiveMeAPotion(ply)
+			GiveMeAPotion(ply)
 	end
 end
+
 
 --This will eventually be a table for the random potion selection to use.
 potions = { "weapon_ttt_healpotion" }
-
-if CLIENT then
-
-end
