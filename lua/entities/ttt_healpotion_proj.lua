89ed926f6ca316850cc17593295db40b5fde2908
EmberTheFlareon <70917055+EmberTheFlareon@users.noreply.github.com>
2021-09-22 00:21:22 -0700

    Added visual splash effect

--- lua/entities/ttt_healpotion_proj.lua
+++ lua/entities/ttt_healpotion_proj.lua
@@ -42,18 +42,42 @@ function ENT:Explode(tr)
 	if SERVER then
 		self:SetNoDraw(true)
 		self:SetSolid(SOLID_NONE)
+
+		-- pull out of the surface
 		if tr.Fraction != 1.0 then
 			self:SetPos(tr.HitPos + tr.HitNormal * 0.6)
 		end
 
 		local pos = self:GetPos()
-		HealRadius(pos, self:GetThrower())
+
+		-- make sure we are removed, even if errors occur later
 		self:Remove()
+
+		-- do your thing
+		HealRadius(pos, self:GetThrower())
+
+		-- flashly
+		local effect = EffectData()
+		effect:SetStart(pos)
+		effect:SetOrigin(pos)
+
+		if tr.Fraction != 1.0 then
+			effect:SetNormal(tr.HitNormal)
+		end
+
+		util.Effect("AntlionGib", effect, true, true)
+
+		-- and LOUD
 		sound.Play(splashsound, pos, 100, 100)
 	else
+		local spos = self:GetPos()
+		local trs = util.TraceLine({start=spos + Vector(0,0,64), endpos=spos + Vector(0,0,-128), filter=self})
+		util.Decal("YellowBlood", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)      
+
 		self:SetDetonateExact(0)
 	end
 end
+
 
 function ENT:PhysicsCollide(data,phys)
 	self:SetDetonateExact(0.1)
--- lua/entities/ttt_jumppotion_proj.lua
+++ lua/entities/ttt_jumppotion_proj.lua
@@ -32,23 +32,46 @@ local function JumpRadius(pos, thrower, ply)
 end
 
 
-local splashsound = Sound("physics/glass/glass_bottle_break2.wav")
 function ENT:Explode(tr)
 	if SERVER then
 		self:SetNoDraw(true)
 		self:SetSolid(SOLID_NONE)
+
+		-- pull out of the surface
 		if tr.Fraction != 1.0 then
 			self:SetPos(tr.HitPos + tr.HitNormal * 0.6)
 		end
 
 		local pos = self:GetPos()
-		JumpRadius(pos, self:GetThrower())
+
+		-- make sure we are removed, even if errors occur later
 		self:Remove()
+
+		-- do your thing
+		JumpRadius(pos, self:GetThrower())
+
+		-- flashly
+		local effect = EffectData()
+		effect:SetStart(pos)
+		effect:SetOrigin(pos)
+
+		if tr.Fraction != 1.0 then
+			effect:SetNormal(tr.HitNormal)
+		end
+
+		util.Effect("AntlionGib", effect, true, true)
+
+		-- and LOUD
 		sound.Play(splashsound, pos, 100, 100)
 	else
+		local spos = self:GetPos()
+		local trs = util.TraceLine({start=spos + Vector(0,0,64), endpos=spos + Vector(0,0,-128), filter=self})
+		util.Decal("YellowBlood", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)      
+
 		self:SetDetonateExact(0)
 	end
 end
+
 
 function ENT:PhysicsCollide(data,phys)
 	self:SetDetonateExact(0.1)
